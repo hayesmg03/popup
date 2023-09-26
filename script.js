7e324a4ceadef817f52ae07cff43823b79dd6cc9
Mitchell Hayes <hayesmg03@gmail.com>
2023-09-25 23:49:16 -0400

    ooo

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const button = document.getElementById("click")
+const body = document.getElementsByName('body')
+console.log(body)
+const style = getComputedStyle(button)
 console.log(button)
 button.addEventListener("click", e => {
     // console.log(e.target)
